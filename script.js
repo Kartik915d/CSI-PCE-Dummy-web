e5e12fe9dcba116d35994020a5b7800135e5c61f
Kartik Dubey <122714675+Kartik915d@users.noreply.github.com>
2026-04-06 23:18:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const toggleBtn = document.getElementById('theme-toggle');
 const toggleIcon = document.getElementById('toggle-icon');
 
 // On load, check for saved theme
-
+const savedTheme = localStorage.getItem('theme') || 'dark';
+document.documentElement.setAttribute('data-theme', savedTheme);
+updateIcon(savedTheme);
 
 toggleBtn.addEventListener('click', () => {
     let currentTheme = document.documentElement.getAttribute('data-theme');

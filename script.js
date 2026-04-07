194d39357cd2ba73f1ce0edbbce564f87b3e1abd
Kartik Dubey <122714675+Kartik915d@users.noreply.github.com>
2026-04-07 21:59:13 +0530

    Add theme toggle functionality with localStorage

--- script.js
+++ script.js
@@ -10,7 +10,9 @@ toggleBtn.addEventListener('click', () => {
     let currentTheme = document.documentElement.getAttribute('data-theme');
     let newTheme = currentTheme === 'dark' ? 'light' : 'dark';
     
-    
+    document.documentElement.setAttribute('data-theme', newTheme);
+    localStorage.setItem('theme', newTheme);
+    updateIcon(newTheme);
 });
 
 function updateIcon(theme) {
